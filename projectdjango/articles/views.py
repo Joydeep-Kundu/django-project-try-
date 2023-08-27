005db4feab7b944e0746cacd0787bb10c6d69eed
Joydeep Kundu <kjoydeep5@gmail.com>
2023-08-27 19:06:04 +0530

    new url and dynamic path handling

--- projectdjango/articles/views.py
+++ projectdjango/articles/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Article
 # Create your views here.
+def article_detail_view(request,id=None):
+    article_obj=None
+    if id:
+        article_obj=Article.objects.get(id=id)
+    context={
+        "object":article_obj
+    }
+    return render(request,"articles/detail.html",context=context)
--- projectdjango/projectdjango/views.py
+++ projectdjango/projectdjango/views.py
@@ -5,12 +5,12 @@ from django.http import HttpResponse
 from django.template.loader import render_to_string,get_template
 from articles.models import Article
 from random import randint
+
 def home_view(request):
     """
     take in a request (Django sends request)
     Return HTM as a response (we pic to return the response)
     """
-    name='rn'
     number=randint(1,3)
     article_obj=Article.objects.get(id=number)
     article_queryset=Article.objects.all()
